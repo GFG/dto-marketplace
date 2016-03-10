29825a3d324389b84fe49306e9c2d0fd61f68fce
Vitor de Souza <vitor.souza@dafiti.com.br>
2016-03-10 16:09:54 -0300

    Remove unused code

--- src/DTOMarketplace/Context/Manager.php
+++ src/DTOMarketplace/Context/Manager.php
@@ -29,45 +29,4 @@ class Manager extends \Context\Manager
         $dataWrapper = new $dataWrapperName($dataWrapperData);
         return parent::build($contextName, $dataWrapper);
     }
-
-    /**
-     * @param array $rawData
-     * @return Manager
-     */
-    public function setRawData($rawData)
-    {
-        $this->rawData = $rawData;
-        return $this;
-    }
-
-    /**
-     * @return array
-     */
-    public function getRawData()
-    {
-        return $this->rawData;
-    }
-
-    /**
-     * undocumented function
-     *
-     * @return Manager
-     */
-    public function setHydrator(Hydrator $hydrator)
-    {
-        $this->hydrator = $hydrator;
-        return $this;
-    }
-
-    /**
-     * @return Hydrator
-     */
-    public function getHydrator()
-    {
-        if (!$this->hydrator) {
-            $this->setHydrator(new Hydrator);
-        }
-
-        return $this->hydrator;
-    }
 }
