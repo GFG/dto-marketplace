1fdee3fd1d7c523834622bfdfacd51db71121bca
Vitor de Souza <vitor.souza@dafiti.com.br>
2016-03-22 10:36:10 -0300

    Fixes Context Manager

--- src/DTOMarketplace/Context/Manager.php
+++ src/DTOMarketplace/Context/Manager.php
@@ -2,20 +2,18 @@
 
 namespace DTOMarketplace\Context;
 
-use DTOMarketplace\DataWrapper;
-use Context\Factory\Hydrator;
-
 class Manager extends \Context\Manager
 {
     /**
      * @param string $contextName
      * @param string $dataWrapperName
      * @param array $datawrapperData
-     *
-     * {@inheritdoc}
      */
-    public function build($contextName, $dataWrapperName, $dataWrapperData)
-    {
+    public function build(
+        $contextName,
+        $dataWrapperName,
+        array $dataWrapperData = []
+    ) {
         $dataWrapper = new $dataWrapperName($dataWrapperData);
         return $this->create($contextName, $dataWrapper);
     }
