733cba058b703e1034986130d7d10aeb7587fa08
Vitor de Souza <vitor.souza@dafiti.com.br>
2017-03-24 14:10:01 -0300

    add intermediate status

---
+++ src/DTOMarketplace/Context/Venture/PostPayment/IntermediateStatus.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace GFG\DTOMarketplace\Context\Venture\PostPayment;
+
+use GFG\DTOMarketplace\Context\Base;
+
+class IntermediateStatus extends Base
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getHttpMethod()
+    {
+        return 'put';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function exportContextData()
+    {
+        $dataWrapper = $this->getDataWrapper();
+        $items = [];
+        foreach ($dataWrapper->getItemCollection() as $item) {
+            $items[] = [
+                'venture_order_item_id' => $item->getVentureOrderItemId(),
+                'order_item_id'         => $item->getOrderItemId()
+            ];
+        }
+
+        return $this->prepareExport([
+            'venture_order_nr' => $dataWrapper->getVentureOrderNr(),
+            'order_nr'         => $dataWrapper->getOrderNr(),
+            'item_collection'  => $items
+        ]);
+    }
+}
